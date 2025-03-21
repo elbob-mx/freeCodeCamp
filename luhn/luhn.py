8f8df9bf43a1986a1c07a374f244e501a036421a
Roberto Gutierrez <rob7th@gmail.com>
2025-03-20 21:53:46 -0600

    luhn update

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -1,2 +1,21 @@
+def verify_card_number(card_number):
+    sum_of_odd_digits = 0
+    card_number_reversed = card_number[::-1]
+    odd_digits = card_number_reversed[::2]
+
+    for digit in odd_digits:
+        sum_of_odd_digits += int(digit)
+    print(sum_of_odd_digits)
+
+    sum_of_even_digits = 0
+    even_digits = card_number_reversed[1::2]
+    for even in even_digits:
+        print(even)
 def main():
-    pass
+    card_number = '4111-1111-4555-1142'
+    card_translation = str.maketrans({'-': '', ' ': ''})
+    translated_card_number = card_number.translate(card_translation)
+
+    verify_card_number(translated_card_number)
+
+main()
