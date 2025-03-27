172e5d0fabf5d7fe353423192c5fd99d0a8b94cd
Roberto Gutierrez <rob7th@gmail.com>
2025-03-26 23:36:41 -0600

    update

---
+++ arithmetic-formatter-project/formatter.py
@@ -0,0 +1,6 @@
+def arithmetic_arranger(problems, show_answers=True):
+    if len(problems) > 5:
+        return "Error: Too many problems."
+    return problems
+
+print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
